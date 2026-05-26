fca45c4e1c25f73010d5de4f5c1c591f5a929a06
Repo-Gint <grupointerconsultti@gmail.com>
2026-05-26 16:22:57 -0600

    feat(interceptors): agregar interceptores de autenticación y manejo de peticiones

--- mantenimiento_test_frontend/src/app/interceptors/auth-interceptor.ts
+++ mantenimiento_test_frontend/src/app/interceptors/auth-interceptor.ts
@@ -1,5 +1,46 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+import { MessagesService } from '../../app/admin/services/messages/messages';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+
+	const router = inject(Router);
+	const messages = inject(MessagesService);
+
+	const token = localStorage.getItem('token_orden_gitlatam');
+
+	let cloned = req;
+
+	if (token) {
+		cloned = req.clone({
+			setHeaders: {
+				Authorization: `Bearer ${token}`
+			}
+		});
+	}
+
+	return next(cloned).pipe(
+		catchError((error: any) => {
+			messages.cerrarMensajes();
+
+			const mensaje = error?.error?.message || 'Ocurrió un error';
+			if (error.status === 401) {
+
+				localStorage.removeItem('token_orden_gitlatam');
+
+				messages.mensajeGenerico(
+					mensaje, 'warning', 'Sesión expirada'
+				);
+
+				router.navigate(['/login']);
+			}
+			else {
+				messages.mensajeGenerico( mensaje, 'error');
+			}
+
+			throw error;
+		})
+	);
 };
