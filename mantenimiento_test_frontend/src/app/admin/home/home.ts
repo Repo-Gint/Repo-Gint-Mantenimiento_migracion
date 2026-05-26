4b602de8aff4cc1bdf781f8f0014b934cf400d0f
Repo-Gint <grupointerconsultti@gmail.com>
2026-05-26 16:03:10 -0600

    feat(layout): integrar navbar y sidenav en estructura principal

--- mantenimiento_test_frontend/src/app/admin/home/home.ts
+++ mantenimiento_test_frontend/src/app/admin/home/home.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { Navbar } from '../components/navbar/navbar';
+import { Sidenav } from '../components/sidenav/sidenav';
+import { SessionService } from '../services/session/session';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [Navbar, Sidenav, RouterOutlet],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+
+  constructor(
+    private sessionService: SessionService
+  ) {}
+
+  ngOnInit(): void {
+    this.sessionService.iniciarContador();
+  }
+
+}
