12f8530d34353de77d949ff327058981ea3d234a
Repo-Gint <grupointerconsultti@gmail.com>
2026-05-26 16:07:16 -0600

    feat(api-usuarios): agregar servicio de API para gestión de usuarios

--- mantenimiento_test_frontend/src/app/admin/services/api/usuarios/usuarios.ts
+++ mantenimiento_test_frontend/src/app/admin/services/api/usuarios/usuarios.ts
@@ -1,6 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { api } from '../../../../../environments/environments';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Usuarios {}
+export class Usuarios {
+  constructor (
+    private http: HttpClient
+  ){}
+
+  	public registrarUsuario(usuario: any): Observable<any> {
+		return this.http.post<any>(`${api}/usuarios/registrarUsuario`, usuario);
+	}
+
+  public obtenerRecursosRegistroUsuario(): Observable<any> {
+    return this.http.get<any>(`${api}/usuarios/obtenerRecursosRegistroUsuario`);
+  }
+
+  public cerrarSesion(): Observable<any> {
+		return this.http.post<any>(`${api}/usuarios/cerrarSesion`, {});
+	}
+}
