383eb88d433b48842be415d0eed28fe02280fa1f
Repo-Gint <grupointerconsultti@gmail.com>
2026-05-26 15:58:30 -0600

    feat(usuarios): se agrego la barra de sidenav

--- mantenimiento_test_frontend/src/app/admin/components/sidenav/sidenav.ts
+++ mantenimiento_test_frontend/src/app/admin/components/sidenav/sidenav.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav',
+  standalone: true,
   imports: [],
   templateUrl: './sidenav.html',
   styleUrl: './sidenav.css',
