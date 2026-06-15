072a98bce764e4986f3500a0f957bc9371a6086b
Repo-Gint <grupointerconsultti@gmail.com>
2026-06-15 17:39:50 -0600

    feat(sidenav): agregar apertura de modales para registro de departamento, área y usuario

--- mantenimiento_test_frontend/src/app/admin/components/sidenav/sidenav.ts
+++ mantenimiento_test_frontend/src/app/admin/components/sidenav/sidenav.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ModalService } from '../../services/modal/modal';
+import { RegistrarUsuario } from '../../modules/usuarios/registrar-usuario/registrar-usuario';
+import { RegistrarArea } from '../../modules/catalogos/areas/registrar-area/registrar-area';
+import { RegistrarDepartamento } from '../../modules/catalogos/departamentos/registrar-departamento/registrar-departamento';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sidenav.html',
   styleUrl: './sidenav.css',
 })
-export class Sidenav {}
+export class Sidenav {
+  constructor(
+    private modal: ModalService
+  ){}
+
+public abrirModalRegistrarUsuario(): void {
+		this.modal.abrirModalConComponente(RegistrarUsuario, {}, 'lg-modal');
+	}
+
+  public abrirModalRegistrarArea(): void {
+		this.modal.abrirModalConComponente(RegistrarArea, {}, 'md-modal');
+	}
+
+  public abrirModalRegistrarDepartamento(): void {
+    this.modal.abrirModalConComponente(RegistrarDepartamento, {}, 'md-moodal')
+  }
+}
