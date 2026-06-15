96630ac63e49bd6c7cd254051a4424041b2636cc
Repo-Gint <grupointerconsultti@gmail.com>
2026-06-15 17:54:30 -0600

    feat(api-areas): agregar servicio con todos los endpoints de áreas

---
+++ mantenimiento_test_frontend/src/app/admin/services/api/areas/areas.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { api } from '../../../../../environments/environments';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AreasService {
+  constructor (
+    private http: HttpClient
+  ) {}
+
+  public registrarArea(area: any): Observable<any> {
+		return this.http.post<any>(`${api}/areas/registrarArea`, area);
+	}
+
+  public obtenerListaAreas(): Observable<any> {
+		return this.http.get<any>(`${api}/areas/obtenerListaAreas`);
+	}
+
+	public obtenerDetalleArea(pkArea: number): Observable<any> {
+		return this.http.get<any>(`${api}/areas/obtenerDetalleArea/${pkArea}`);
+	}
+
+	public actualizarArea(area: any): Observable<any> {
+		return this.http.put<any>(`${api}/areas/actualizarArea`, area);
+	}  
+
+	public cambiarStatusArea(pkArea: number): Observable<any> {
+		return this.http.get<any>(`${api}/areas/cambiarStatusArea/${pkArea}`)
+	}
+
+}
