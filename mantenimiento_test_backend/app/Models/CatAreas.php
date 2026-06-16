1f9e4d081e8dde246c4c973d19af33979814c374
Repo-Gint <grupointerconsultti@gmail.com>
2026-06-16 08:58:49 -0600

    feat(models): definir CatAreas con campos fillable

---
+++ mantenimiento_test_backend/app/Models/CatAreas.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CatAreas extends Model {
+    use HasFactory;
+    public $timestamps    = false;
+    protected $primaryKey = 'id_area';
+    protected $table      = 'cat_areas';
+
+    protected $fillable = [
+        'id_area',
+        'area',
+        'acronym',
+        'color',
+        'active'
+    ];
+}
