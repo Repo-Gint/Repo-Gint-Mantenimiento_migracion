11b55bab8f322012e3ecf9451ea8251fc392426d
Repo-Gint <grupointerconsultti@gmail.com>
2026-06-16 12:00:55 -0600

    feat(repository-maquinas): agregar métodos de obtencion de lista, detalle, actualización y cambio de estado

--- mantenimiento_test_backend/app/Repositories/Admin/Catalogos/MaquinasRepository.php
+++ mantenimiento_test_backend/app/Repositories/Admin/Catalogos/MaquinasRepository.php
@@ -19,11 +19,85 @@ use Illuminate\Support\Facades\DB;
                 $registro->voltaje   = $maquinas['voltaje'];
                 $registro->amperage  = $maquinas['amperage'];
                 $registro->frequency = $maquinas['frequency'];
-                $registro->kva = $maquinas['kva'];
+                $registro->kva       = $maquinas['kva'];
                 $registro->active    = 1;
                 $registro->save();
 
                 return $registro->id_machines;
+            }
+
+            public function obtenerListaMaquinas() {
+                $query = CatMaquinas::select(
+                    'id_machines',
+                    'machines',
+                    'id_area',
+                    'brand',
+                    'model',
+                    'year',
+                    'serial',
+                    'weight',
+                    'voltaje',
+                    'amperage',
+                    'frequency',
+                    'kva', 
+                    'active',
+                    DB::raw("               CASE 
+                                                WHEN active = 1 THEN 'Activo'
+                                                ELSE 'Inactivo'
+                                                END as estado
+                    ")
+                );
+
+                return $query->get();
+            }
+
+            public function obtenerDetalleMaquina($pkMaquina) {
+                $query = CatMaquinas::select(
+                    'id_machines',
+                    'machines',
+                    'id_area',
+                    'brand',
+                    'model',
+                    'year',
+                    'serial',
+                    'weight',
+                    'voltaje',
+                    'amperage',
+                    'frequency',
+                    'kva',
+                    'active',
+                )
+                    ->where([
+                        ['id_machines', $pkMaquina],
+                        ['active', 1]
+                    ]);
+
+                    return $query->get();
+            }
+            
+            public function actualizarMaquina($id, $maquina) {
+                $actualizar = CatMaquinas::findOrFail($id);
+
+                $actualizar->machines    = $maquina['machines'];
+                $actualizar->id_area     = $maquina['id_area'];
+                $actualizar->brand       = $maquina['brand'];
+                $actualizar->model       = $maquina['model'];
+                $actualizar->year        = $maquina['year'];
+                $actualizar->serial      = $maquina['serial'];
+                $actualizar->weight      = $maquina['weight'];
+                $actualizar->voltaje     = $maquina['voltaje'];
+                $actualizar->amperage    = $maquina['amperage'];
+                $actualizar->frequency   = $maquina['frequency'];
+                $actualizar->kva         = $maquina['kva'];
+                $actualizar->save();
+            }
+
+            public function cambiarStatusMaquina($pkMaquina) {
+                $maquinas         = CatMaquinas::findOrFail($pkMaquina);
+                $maquinas->active = $maquinas->active ? 0 :1;
+                $maquinas->save();
+
+                return $maquinas->active;
             }
         }
     
