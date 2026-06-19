abf7ec91798641997c361734c1850e701fda704a
Repo-Gint <grupointerconsultti@gmail.com>
2026-06-19 09:01:38 -0600

    feat(models): definir CatTipoMantenimiento con campos fillable

---
+++ mantenimiento_test_backend/app/Models/CatTipoMantenimiento.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CatTipoMantenimiento extends Model {
+    use HasFactory;
+    public $timestamps = false;
+    protected $primaryKey = 'id_type_maintenances';
+    protected $table      = 'cat_type_maintenances';
+
+    protected $fillable = [
+        'id_type_maintenances', 
+        'type_maintances',
+        'color',
+        'acronym',
+        'active'
+    ];
+}
